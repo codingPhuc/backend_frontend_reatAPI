987aedc3f708c1c2afe07f28d0ed02ccfd3da63e
Nguyen Hoang Phuc <phucnguyenhoang3839@gmail.com>
2024-12-19 16:18:05 +0700

    4_TransformingFetchData_React

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Places from "./Places.jsx";
 import { useEffect } from "react";
 import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
 export default function AvailablePlaces({ onSelectPlace }) {
   // we cannot used async await here
   // const respone  = await fetch('https://localhost:3000/places')   ;
@@ -13,14 +14,19 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
 
       try {
-        const respone = await fetch("http://localhost:3000/placess");
+        const respone = await fetch("http://localhost:3000/places");
         const resData = await respone.json();
         if (!respone.ok) {
           throw new Error("Failed to fetch places");
         }
         navigator.geolocation.getCurrentPosition((position) => {
-          setAvailablePlaces(resData.places); 
-          
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
         });
       } catch (error) {
         setError({
@@ -28,8 +34,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
             error.message ||
             "Could  not fetch places , please try again latter",
         });
+        setIsFetching(false);
       }
-      setIsFetching(false);
     }
     fetchPlaces();
   }, []);
